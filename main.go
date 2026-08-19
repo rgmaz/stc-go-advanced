30106bbd61fa5a934562f493a940eeae94baddb3
Ryan G. Mazambani <237030736+rgmaz@users.noreply.github.com>
2026-08-19 21:52:24 +0200

    Error Wrapping and errors.Is /errors.As

--- main.go
+++ main.go
@@ -1,24 +1,36 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
-	"reflect"
+	"os"
+	"strconv"
 )
 
-type Person struct {
-	Name string
-	Age  int
+var ErrTooSmall = errors.New("value too small")
+
+func validate(n int) error {
+	if n >= 10 {
+		return nil
+	}
+
+	return fmt.Errorf("validating n=%d: %w", n, ErrTooSmall)
 }
 
 func main() {
-	p := Person{Name: "Ada", Age: 36}
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	n, _ := strconv.Atoi(sc.Text())
 
-	t := reflect.TypeOf(p)
-	v := reflect.ValueOf(p)
-
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		val := v.Field(i)
-		fmt.Printf("%s: %v\n", field.Name, val.Interface())
+	err := validate(n)
+	if err != nil {
+		if errors.Is(err, ErrTooSmall) {
+			fmt.Printf("too small: %d", n)
+			return
+		}
+		fmt.Printf("error: %v", err)
+		return
 	}
+	fmt.Println("ok")
 }
