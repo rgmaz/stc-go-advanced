baa3ed214e15486a0a0d0f7507332b40d0b0a8d2
Ryan G. Mazambani <237030736+rgmaz@users.noreply.github.com>
2026-08-19 17:37:51 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+import (
+    "bufio"
+    "context"
+    "fmt"
+    "os"
+    "strconv"
+    "time"
+)
+
+func sumWithCtx(ctx context.Context, nums []int) (int, error) {
+    total := 0
+    for _, n := range nums {
+        // select on ctx.Done
+        _ = n
+    }
+    _ = time.Millisecond
+    return total, nil
+}
+
+func main() {
+    sc := bufio.NewScanner(os.Stdin)
+    sc.Scan(); n, _ := strconv.Atoi(sc.Text())
+    nums := make([]int, n)
+    for i := 0; i < n; i++ {
+        sc.Scan()
+        nums[i], _ = strconv.Atoi(sc.Text())
+    }
+    ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+    defer cancel()
+    total, err := sumWithCtx(ctx, nums)
+    if err != nil {
+        fmt.Println("error:", err)
+    } else {
+        fmt.Println(total)
+    }
+}
